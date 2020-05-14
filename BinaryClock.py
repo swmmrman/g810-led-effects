0c5b86dbf5695b497780602da59b61515beea7d6
Stephan Kern <stephan@swmmrman.com>
2020-05-14 07:43:54 -0600

    Made Hour minutes and seconds individual color, moved keys again

--- BinaryClock.py
+++ BinaryClock.py
@@ -6,26 +6,27 @@ import time
 import sys
 
 UPDATE_TIME = 1 # Time in seconds.
-SEP_COLOR = "FFEEDD"
-HOUR_COLOR = "FF0000"
-MINUTE_COLOR = "00FF00"
-SECOND_COLOR = "0000FF"
-OFF_COLOR = "000000"
-BLOCK_COLOR = "993F1F"
-exitColor = "993F1F"
 
+COLORS = {
+    'hour'      : "FF0000",
+    'minute'    : "00FF00",
+    'second'    : "0000FF",
+    'seperator' : "FFEEDD",
+    'off'       : "000000",
+    'block'     : "993F1F",
+    'exit'      : "993F1F",
+}
 KEYS  = {
-    'hour': ['1','2','3','4','5'],
-    'minute' : ['7','8','9','0','-','='],
-    'second' : ['d', 'f', 'g', 'h', 'j', 'k'],
-    "seprator" : '6',
+    'hour'     : ['1','2','3','4','5'],
+    'minute'   : ['7','8','9','0','-','='],
+    'second'   : ['c', 'v', 'b', 'n', 'm', ','],
+    "seperator" : '6',
 }
 
 def SetKeys():
     """Set the required keys to on or off."""
     command_string = "echo -e '\n"
-    off_keys = []
-    on_keys = []
+    keys = []
     t = time.localtime()
     td = {
         'hour' : t.tm_hour,
@@ -35,25 +36,21 @@ def SetKeys():
     for k in td:
         for i,key in enumerate(KEYS[k][::-1]):
             if td[k] & (1<<i):
-                on_keys.append(key)
+                command_string += F"k {key} {COLORS[k]}\n"
             else:
-                off_keys.append(key)
-    for key in on_keys:
-        command_string += F"k {key} {HOUR_COLOR}\n"
-    for key in off_keys:
-        command_string += F"k {key} {OFF_COLOR}\n"
+                command_string += f"k {key} {COLORS['off']}\n"
     command_string += "c'"
     subprocess.call(F"{command_string} | g810-led -pp", shell=True)
 
 if len(sys.argv) > 1:
     print("Usage python3 BinaryClock.py")
     sys.exit(1)
-subprocess.call(F"g910-led -a {BLOCK_COLOR}", shell=True)
-subprocess.call(F"g910-led -k {KEYS['seprator']} {SEP_COLOR}", shell=True)
+subprocess.call(F"g910-led -a {COLORS['block']}", shell=True)
+subprocess.call(F"g910-led -k {KEYS['seperator']} {COLORS['seperator']}", shell=True)
 try:
     while True:
         SetKeys()
         time.sleep(1)
 except KeyboardInterrupt:
-    subprocess.call(f"g910-led -a {exitColor}", shell=True)
+    subprocess.call(f"g910-led -a {COLORS['exit']}", shell=True)
 print("\033[A")
