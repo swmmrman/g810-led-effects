bae1bd44d53444dba6ab12ab33c7761b9756188f
Stephan Kern <stephan@swmmrman.com>
2020-05-14 09:01:24 -0600

    Follow Pyton styles

--- BinaryClock.py
+++ BinaryClock.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
-""" This is a crappy binary clock using the keyboard lights. """
+"""This is a crappy binary clock using the keyboard lights."""
 
 import subprocess
 import time
 import sys
 
-UPDATE_TIME = 1 # Time in seconds.
+
+UPDATE_TIME = 1  # Time in seconds.
 COLORS = {
-    'month'     : "FFFF00",
-    'day'       : "00FFFF",
-    'hour'      : "FF0000",
-    'minute'    : "00FF00",
-    'second'    : "0000FF",
-    'seperator' : "FFEEDD",
-    'off'       : "000000",
-    'block'     : "993F1F",
-    'exit'      : "993F1F",
+    'month':     "FFFF00",
+    'day':       "00FFFF",
+    'hour':      "FF0000",
+    'minute':    "00FF00",
+    'second':    "0000FF",
+    'seperator': "FFEEDD",
+    'off':       "000000",
+    'block':     "993F1F",
+    'exit':      "993F1F",
 }
-KEYS  = {
-    'month'    : ['G6', 'G7', 'G8', 'G9'],
-    'day'      : ['G1', 'G2', 'G3', "G4", "G5"],
-    'hour'     : ['1', '2', '3', '4', '5'],
-    'minute'   : ['7', '8', '9', '0', '-', '='],
-    'second'   : ['c', 'v', 'b', 'n', 'm', ','],
-    "seperator" : '6',
+KEYS = {
+    'month':     ['G6', 'G7', 'G8', 'G9'],
+    'day':       ['G1', 'G2', 'G3', "G4", "G5"],
+    'hour':      ['1', '2', '3', '4', '5'],
+    'minute':    ['7', '8', '9', '0', '-', '='],
+    'second':    ['c', 'v', 'b', 'n', 'm', ','],
+    "seperator": '6',
 }
+
 
 def SetKeys():
     """Set the required keys to on or off."""
@@ -32,26 +34,29 @@ def SetKeys():
     keys = []
     t = time.localtime()
     td = {
-        'month'  : t.tm_mon,
-        'day'    : t.tm_mday,
-        'hour'   : t.tm_hour,
-        'minute' : t.tm_min,
-        'second' : t.tm_sec,
+        'month':    t.tm_mon,
+        'day':      t.tm_mday,
+        'hour':     t.tm_hour,
+        'minute':   t.tm_min,
+        'second':   t.tm_sec,
     }
     for k in td:
-        for i,key in enumerate(KEYS[k][::-1]):
-            if td[k] & (1<<i):
+        for i, key in enumerate(KEYS[k][::-1]):
+            if td[k] & (1 << i):
                 command_string += F"k {key} {COLORS[k]}\n"
             else:
                 command_string += f"k {key} {COLORS['off']}\n"
     command_string += "c'"
     subprocess.call(F"{command_string} | g810-led -pp", shell=True)
 
+
 if len(sys.argv) > 1:
     print("Usage python3 BinaryClock.py")
+    subprocess.call(F"g810-led -a {COLORS['block']}", shell=True)
     sys.exit(1)
-subprocess.call(F"g810-led -a {COLORS['block']}", shell=True)
-subprocess.call(F"g810-led -k {KEYS['seperator']} {COLORS['seperator']}", shell=True)
+subprocess.call(
+    F"g810-led -k {KEYS['seperator']} {COLORS['seperator']}", shell=True
+)
 try:
     while True:
         SetKeys()
